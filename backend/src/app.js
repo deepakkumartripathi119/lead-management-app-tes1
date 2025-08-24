510a25895e99c2e1edfc6e874dd86700d3210318
deepaktripathi119 <deepaktripathi14022005@gmail.com>
2025-08-24 07:39:46 +0530

    fdb

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['http://localhost:3000','https://lead-management-app1.vercel.app/'], 
+  origin: ['https://lead-management-app1.vercel.app', 'http://localhost:3000'], 
   credentials: true, 
 }));
 
