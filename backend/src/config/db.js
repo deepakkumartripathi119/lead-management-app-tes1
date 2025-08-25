672de47ed34f36744e51e7c02e8101d3061d17c5
deepaktripathi119 <deepaktripathi14022005@gmail.com>
2025-08-25 10:20:47 +0530

    csdcsc

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
     try {
-        const MONG_URL = (process.env.PORT===5000)?process.env.MONGO_URI_PRODUCTION:process.env.MONGO_URI_LOCAL;
+        const MONG_URL = (process.env.NODE_ENV==='production')?process.env.MONGO_URI_PRODUCTION:process.env.MONGO_URI_LOCAL;
         const conn = await mongoose.connect(MONG_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
